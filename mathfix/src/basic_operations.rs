f4e103ab10440cc8de0491fff72d911dd88fc031
Daniel <daniel.cordeiro2@fatec.sp.gov.br>
2026-01-27 17:39:21 -0300

    ✅ test: Adiciona testes

--- mathfix/src/basic_operations.rs
+++ mathfix/src/basic_operations.rs
@@ -58,3 +58,62 @@ impl Basic {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn add_u64_ok() {
+        let mut result: u64 = 0;
+        let operation = Basic::add_u64(2, 23, &mut result);
+        assert_eq!(25, result);
+        assert_eq!(StatusCode::Ok, operation);
+    }
+
+    #[test]
+    fn add_overflow() {
+        let mut result: u64 = 0;
+        let operation = Basic::add_u64(u64::MAX, 1, &mut result);
+        assert_eq!(StatusCode::Overflow, operation);
+    }
+
+    #[test]
+    fn sub_u64_ok() {
+        let mut result: u64 = 0;
+        let operation: StatusCode = Basic::subtract_u64(255, 2, &mut result);
+        assert_eq!(253, result);
+        assert_eq!(StatusCode::Ok, operation);
+    }
+
+    #[test]
+    fn mul_u64_ok() {
+        let mut result: u64 = 0;
+        let operation: StatusCode = Basic::multiply_u64(255, 2, &mut result);
+        assert_eq!(510, result);
+        assert_eq!(StatusCode::Ok, operation);
+    }
+
+    #[test]
+    fn mult_overflow() {
+        let mut result: u64 = 0;
+        let operation: StatusCode = Basic::multiply_u64(u64::MAX, 2, &mut result);
+        assert_eq!(StatusCode::Overflow, operation);
+    }
+
+    #[test]
+    fn div_ok() {
+        let mut result: u64 = 0;
+        let operation: StatusCode = Basic::divide_u64(255, 2, &mut result);
+        assert_eq!(127, result);
+        assert_eq!(StatusCode::Ok, operation);
+    }
+
+    #[test]
+    fn div_zero() {
+        let mut result: u64 = 0;
+        let operation: StatusCode = Basic::divide_u64(1, 0, &mut result);
+        assert_eq!(StatusCode::DivisionByZero, operation);
+    }
+}
