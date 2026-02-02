3831e24dbfaf5c77fa2af0268557769dd0490aa7
Daniel <daniel.cordeiro2@fatec.sp.gov.br>
2026-02-02 18:47:15 -0300

    ✨ feat: Implementa chamadas funcoes da lib

--- java-test/src/main/java/com/mathfix/Main.java
+++ java-test/src/main/java/com/mathfix/Main.java
@@ -2,6 +2,20 @@ package com.mathfix;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Mathfix...");
+
+        long[] res_soma = new long[1];
+        long[] res_subtracao = new long[1];
+        long[] res_multiplicacao = new long[1];
+        long[] res_divisao = new long[1];
+
+        MathfixLib.INSTANCE.add_u64(100,200, res_soma);
+        MathfixLib.INSTANCE.subtract_u64(1000,200, res_subtracao);
+        MathfixLib.INSTANCE.multiply_u64(100,200, res_multiplicacao);
+        MathfixLib.INSTANCE.divide_u64(1000,200, res_divisao);
+
+        System.out.println("Soma: "+res_soma[0]);
+        System.out.println("Subtracao: "+res_subtracao[0]);
+        System.out.println("Multiplicacao: "+res_multiplicacao[0]);
+        System.out.println("Divisao: "+res_divisao[0]);
     }
 }
